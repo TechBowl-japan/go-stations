cf60178e75e3029efcdbe8d3a1647dc4473f11fe
Takuma Kajikawa <kj1ktk@gmail.com>
2022-07-17 14:18:15 +0900

    st5

--- handler/healthz.go
+++ handler/healthz.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/TechBowl-japan/go-stations/model"
@@ -16,5 +18,11 @@ func NewHealthzHandler() *HealthzHandler {
 
 // ServeHTTP implements http.Handler interface.
 func (h *HealthzHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	_ = &model.HealthzResponse{}
+	res := &model.HealthzResponse{
+		Message: "OK",
+	}
+	err := json.NewEncoder(w).Encode(res)
+	if err != nil {
+		log.Println(err)
+	}
 }
