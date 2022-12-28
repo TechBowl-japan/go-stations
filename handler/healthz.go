21b4a6e76b631cfa7eeb79f0ffc1961df33c67d7
tatara-takumi <tataratakumi@gmail.com>
2022-12-28 19:01:09 +0900

    station-05

--- handler/healthz.go
+++ handler/healthz.go
@@ -1,13 +1,15 @@
 package handler
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/TechBowl-japan/go-stations/model"
 )
 
 // A HealthzHandler implements health check endpoint.
-type HealthzHandler struct{}
+type HealthzHandler struct {
+}
 
 // NewHealthzHandler returns HealthzHandler based http.Handler.
 func NewHealthzHandler() *HealthzHandler {
@@ -16,5 +18,7 @@ func NewHealthzHandler() *HealthzHandler {
 
 // ServeHTTP implements http.Handler interface.
 func (h *HealthzHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	_ = &model.HealthzResponse{}
+	res := &model.HealthzResponse{Message: "OK"}
+	json.NewEncoder(w).Encode(res)
+
 }
