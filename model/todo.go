48fad23bf98026153fe10087bf788dcc2d842f00
Rocky-6 <imu387058@gmail.com>
2023-05-16 18:02:46 +0900

    6

--- model/todo.go
+++ model/todo.go
@@ -1,8 +1,16 @@
 package model
 
+import "time"
+
 type (
 	// A TODO expresses ...
-	TODO struct{}
+	TODO struct {
+		ID          int       `json:"id"`
+		Subject     string    `json:"subject"`
+		Description string    `json:"description"`
+		CreatedAt   time.Time `json:"created_at"`
+		UpdatedAt   time.Time `json:"updated_at"`
+	}
 
 	// A CreateTODORequest expresses ...
 	CreateTODORequest struct{}
