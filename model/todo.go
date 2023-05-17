8e297b8c8c817bc3929d501f7f899c85dddb334b
Rocky-6 <imu387058@gmail.com>
2023-05-18 00:01:29 +0900

    17

--- model/todo.go
+++ model/todo.go
@@ -44,7 +44,9 @@ type (
 	}
 
 	// A DeleteTODORequest expresses ...
-	DeleteTODORequest struct{}
+	DeleteTODORequest struct {
+		IDs []int64 `json:"ids"`
+	}
 	// A DeleteTODOResponse expresses ...
 	DeleteTODOResponse struct{}
 )
