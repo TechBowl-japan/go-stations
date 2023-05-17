20df5bd44639b49a02234d97f437396450dd158f
Rocky-6 <imu387058@gmail.com>
2023-05-17 22:00:36 +0900

    12

--- service/todo.go
+++ service/todo.go
@@ -66,7 +66,25 @@ func (s *TODOService) UpdateTODO(ctx context.Context, id int64, subject, descrip
 		confirm = `SELECT subject, description, created_at, updated_at FROM todos WHERE id = ?`
 	)
 
-	return nil, nil
+	result, err := s.db.ExecContext(ctx, update, subject, description, id)
+	if err != nil {
+		return nil, err
+	}
+
+	row, err := result.RowsAffected()
+	if err != nil {
+		return nil, err
+	}
+	if row == 0 {
+		return nil, &model.ErrNotFound{}
+	}
+
+	todo := &model.TODO{ID: id}
+	err = s.db.QueryRowContext(ctx, confirm, id).Scan(&todo.Subject, &todo.Description, &todo.CreatedAt, &todo.UpdatedAt)
+	if err != nil {
+		return nil, err
+	}
+	return todo, nil
 }
 
 // DeleteTODO deletes TODOs on DB by ids.
