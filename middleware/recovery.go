5d65aca4a062adbc218673d51730fabf67108d51
tfuji <tomofuji.dev@gmail.com>
2023-10-29 09:52:26 +0900

    recovery func

--- middleware/recovery.go
+++ middleware/recovery.go
@@ -2,11 +2,17 @@ package middleware
 
 import "net/http"
 
+// h.ServerHTTPの実行中にパニックが発生した場合に、500 Internal Server Errorを返す
 func Recovery(h http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
-		// TODO: ここに実装をする
+		defer func() {
+			// recover(): panic()を捕捉する
+			if err := recover(); err != nil {
+				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			}
+		}()
+		h.ServeHTTP(w, r)
 	}
-	// TODO: hを呼び出す必要がある
 	// http.HandlerFuncは、func(w http.ResponseWriter, r *http.Request)のような関数をhttp.Handlerに変換する
 	return http.HandlerFunc(fn)
 }
