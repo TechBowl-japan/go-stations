23540cc0bd6c6c5a4d14fe9c54aaee68df1f1dd3
Tetsuhisa00 <tetsuhisadevelop@gmail.com>
2023-11-23 20:15:02 +0900

    complete station17

--- model/todo.go
+++ model/todo.go
@@ -47,7 +47,11 @@ type (
 	}
 
 	// A DeleteTODORequest expresses ...
-	DeleteTODORequest struct{}
+	DeleteTODORequest struct{
+		IDs []int64 `json:"ids"`
+	}
 	// A DeleteTODOResponse expresses ...
-	DeleteTODOResponse struct{}
+	DeleteTODOResponse struct{
+		TODOs []*TODO `json:"todos"`
+	}
 )
