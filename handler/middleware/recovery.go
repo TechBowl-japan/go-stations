6c5175640f163046c81ae656f22a405bd5868b0e
Tetsuhisa00 <tetsuhisadevelop@gmail.com>
2023-12-09 21:59:04 +0900

    add ServeHTTP in recovery func

--- handler/middleware/recovery.go
+++ handler/middleware/recovery.go
@@ -5,6 +5,13 @@ import "net/http"
 func Recovery(h http.Handler) http.Handler {
 fn := func(w http.ResponseWriter, r *http.Request) {
 // TODO: ここに実装をする
+
+	// 前処理をここに実装する
+
+	h.ServeHTTP(w, r)
+
+	//後処理をここに実装する
+	
 }
 return http.HandlerFunc(fn)
 }
