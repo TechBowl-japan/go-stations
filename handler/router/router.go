37ef07b9c7c15d5cc76745d703f46a028dcebcc4
Tetsuhisa00 <tetsuhisadevelop@gmail.com>
2023-12-10 14:34:25 +0900

    replace panicHandler with middleware.Recovery and panicHandler is set up as an argument of middleware.Recovery

--- handler/router/router.go
+++ handler/router/router.go
@@ -3,18 +3,22 @@ package router
 import (
 	"database/sql"
 	"net/http"
-
+   
 	"github.com/TechBowl-japan/go-stations/handler"
 	"github.com/TechBowl-japan/go-stations/service"
+	"github.com/TechBowl-japan/go-stations/handler/middleware"
+	
 )
 
 func NewRouter(todoDB *sql.DB) *http.ServeMux {
 	
 	todoService := service.NewTODOService(todoDB)
 	todoHandler := handler.NewTODOHandler(todoService)
+	panicHandler := handler.NewPanicHandler()
 
 	mux := http.NewServeMux()
 	mux.Handle("/healthz", &handler.HealthzHandler{})
 	mux.Handle("/todos", todoHandler)
+	mux.Handle("/do-panic", middleware.Recovery(panicHandler))
 	return mux
 }
