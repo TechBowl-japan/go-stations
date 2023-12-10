e8e2fe5a45006361e4db65d4b9305059967abc50
Tetsuhisa00 <tetsuhisadevelop@gmail.com>
2023-12-10 14:31:57 +0900

    add code that is recovery func to allows the application to work even if panic happend

--- handler/middleware/recovery.go
+++ handler/middleware/recovery.go
@@ -1,17 +1,24 @@
 package middleware
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func Recovery(h http.Handler) http.Handler {
 fn := func(w http.ResponseWriter, r *http.Request) {
 // TODO: ここに実装をする
 
 	// 前処理をここに実装する
-
+    defer func() {
+		if err := recover(); err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	}()
 	h.ServeHTTP(w, r)
 
-	//後処理をここに実装する
-	
+
 }
 return http.HandlerFunc(fn)
 }
