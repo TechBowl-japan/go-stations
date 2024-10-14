e91012289da7d6eae8520dddf8b12bc8a418c503
Rocky-6 <imu387058@gmail.com>
2024-10-14 18:13:52 +0900

    fix

--- handler/middleware/recovery.go
+++ handler/middleware/recovery.go
@@ -1,15 +1,16 @@
 package middleware
 
 import (
+	"log"
 	"net/http"
 )
 
 func Recovery(h http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
-		// TODO: ここに実装をする
 		defer func() {
 			if err := recover(); err != nil {
-				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+				log.Println(err)
+				w.WriteHeader(http.StatusInternalServerError)
 			}
 		}()
 		h.ServeHTTP(w, r)
