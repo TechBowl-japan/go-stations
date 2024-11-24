c5009b0f11b43fb3a7c3ad936678b8177b85cd09
TakahitoTomosue <takataka20030929@gmail.com>
2024-11-24 19:01:05 +0900

    TECHTRAIN RAILWAY Taka station 13~14

--- handler/todo.go
+++ handler/todo.go
@@ -30,6 +30,8 @@ func (h *TODOHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodPost: //POSTメソッドの場合
 		h.handleCreate(w, r) //TODO作成の処理を呼び出す
+	case http.MethodPut: //PUTメソッドの場合
+		h.handleUpdate(w, r) //TODO編集の処理を呼び出す
 	default:
 		//他のメソッドは許可されていないため、エラーレスポンスを返す
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
@@ -94,10 +96,65 @@ func (h *TODOHandler) Read(ctx context.Context, req *model.ReadTODORequest) (*mo
 	return &model.ReadTODOResponse{}, nil
 }
 
+// handleUpdate handles the PUT request to update an existing TODO.
+// handleUpdateは、既存のTODOを変更するためのPUTリクエストを処理する。
+func (h *TODOHandler) handleUpdate(w http.ResponseWriter, r *http.Request) {
+	//リクエストボディを解析し、UpdateTODORequest構造体にデコードする。
+	var req model.UpdateTODORequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		log.Printf("Error decoding UpdateTODORequest: %v", err)
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	defer r.Body.Close() //リクエストボディをクローズする
+
+	//必須フィールドが正しいかをチェックをする。
+	if req.ID == 0 || req.Subject == "" {
+		//IDが0かSubjectが空の場合、400BadRequestを返す
+		http.Error(w, "Invalid ID or Subject", http.StatusBadRequest)
+		return
+	}
+
+	//Contextを取得し、Updateメソッドを呼び出してTODOを更新する。
+	ctx := r.Context()
+	res, err := h.Update(ctx, &req)
+	if err != nil {
+		//TODOが見つからなかった場合
+		if _, ok := err.(*model.ErrNotFound); ok {
+			http.Error(w, "TODO not found", http.StatusNotFound)
+			return
+		}
+
+		//その他のエラーが発生した場合、500Internal Server Errorを返す
+		log.Printf("Error updating TODO: %v", err)
+		http.Error(w, "Failed to update TODO", http.StatusInternalServerError)
+		return
+	}
+
+	//レスポンスヘッダを設定し、成功ステータス(200 OK)を返す
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(res); err != nil {
+		//レスポンスヘッダのエンコードに失敗した場合、500Internal Server Errorを返す
+		http.Error(w, "Faild to encode JSON", http.StatusInternalServerError)
+	}
+}
+
 // Update handles the endpoint that updates the TODO.
+// UpdateはTODOの更新を行うエンドポイントを処理します。
 func (h *TODOHandler) Update(ctx context.Context, req *model.UpdateTODORequest) (*model.UpdateTODOResponse, error) {
-	_, _ = h.svc.UpdateTODO(ctx, 0, "", "")
-	return &model.UpdateTODOResponse{}, nil
+	//TODOServiceのUpdateTODOメソッドを呼び出してTODOを更新する
+	todo, err := h.svc.UpdateTODO(ctx, req.ID, req.Subject, req.Description)
+	if err != nil {
+		//更新中にエラーが発生した場合、そのエラーを呼び出し元に返す。
+		return nil, err
+	}
+
+	//更新されたTODOをレスポンスとして返す
+	return &model.UpdateTODOResponse{
+		TODO: *todo,
+	}, nil
 }
 
 // Delete handles the endpoint that deletes the TODOs.
