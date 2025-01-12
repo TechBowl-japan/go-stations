24dba236ced6ee514f9e9dfc9274b8242c2a71df
Sugar1201 <140928.miina@gmail.com>
2025-01-12 12:01:29 +0900

    Implementing Request&Response method

--- model/todo.go
+++ model/todo.go
@@ -1,13 +1,26 @@
 package model
 
-type (
-	// A TODO expresses ...
-	TODO struct{}
+import "time"
 
-	// A CreateTODORequest expresses ...
-	CreateTODORequest struct{}
-	// A CreateTODOResponse expresses ...
-	CreateTODOResponse struct{}
+type (
+	// A TODO expresses a TODO item.
+	TODO struct{
+		ID          int       `json:"id"`          // JSONキーは "id"
+		Subject     string    `json:"subject"`     // JSONキーは "subject"
+		Description string    `json:"description"` // JSONキーは "description"
+		CreatedAt   time.Time `json:"created_at"`  // JSONキーは "created_at"
+		UpdatedAt   time.Time `json:"updated_at"`  // JSONキーは "updated_at"
+	}
+
+	// A CreateTODORequest expresses the request to create a new TODO item.
+	CreateTODORequest struct{
+		Subject     string `json:"subject"`     // 必須フィールド
+		Description string `json:"description"` // 任意のフィールド
+	}
+	// A CreateTODOResponse expresses the response after creating a new TODO item.
+	CreateTODOResponse struct{
+		TODO TODO `json:"todo"` // 作成された TODO
+	}
 
 	// A ReadTODORequest expresses ...
 	ReadTODORequest struct{}
